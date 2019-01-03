ed6f5966a726e3446eec5b9aac15fe42bd7168eb
Debangshu Dasgupta <Debangshu.Dasgupta@rci.com>
2019-01-03 16:08:30 -0500

    refactored

--- employee-service/src/main/java/org/dasgupta/sample/springboot/webflux/SampleSpringBootWebfluxApplication.java
+++ employee-service/src/main/java/org/dasgupta/sample/springboot/webflux/SampleSpringBootWebfluxApplication.java
@@ -2,8 +2,10 @@ package org.dasgupta.sample.springboot.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SampleSpringBootWebfluxApplication {
 
     public static void main(String[] args) {
--- zipkin-distributed-tracing-service/src/main/java/org/dasgupta/sample/springcloud/service/tracing/ZipkinDistributedTracingServerApplication.java
+++ zipkin-distributed-tracing-service/src/main/java/org/dasgupta/sample/springcloud/service/tracing/ZipkinDistributedTracingServerApplication.java
@@ -2,9 +2,11 @@ package org.dasgupta.sample.springcloud.service.tracing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableZipkinServer
 public class ZipkinDistributedTracingServerApplication {
 
