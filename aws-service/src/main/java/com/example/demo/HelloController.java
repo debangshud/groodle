69ba6ec0937a11430045d05c4e8f2fbbc3c63f44
Debangshu Dasgupta <debangshu.dasgupta@gmail.com>
2019-05-02 20:42:03 -0400

    Updated

---
+++ aws-service/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello() throws UnknownHostException {
+
+        String hostName = Inet4Address.getLocalHost().getHostName();
+
+        return "Hello from " + hostName;
+    }
+}
