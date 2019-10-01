ad96193911d00b7e2f42e569df9f343b2ce487ab
Debangshu Dasgupta <Debangshu.Dasgupta@gmail.com>
2019-10-01 16:29:51 -0400

    cleaned

--- sample-spring-boot-jpa/src/main/java/org/dasgupta/sample/springboot/jpa/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package org.dasgupta.sample.springboot.jpa.repository;
-
-import org.dasgupta.sample.springboot.jpa.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
--- sample-spring-boot-jpa/src/main/java/org/dasgupta/sample/springboot/jpa/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.dasgupta.sample.springboot.jpa.repository;
-
-import org.dasgupta.sample.springboot.jpa.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,String> {
-    User findByUsername(String username);
-}
