9db1bb3c375b1143f0f297b44fcf7a0bd346a8ce
Debangshu Dasgupta <Debangshu.Dasgupta@gmail.com>
2019-10-03 20:50:14 -0400

    cleaned

--- sample-spring-boot-mongodb/src/main/java/org/dasgupta/sample/springboot/mongodb/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package org.dasgupta.sample.springboot.mongodb.repositories;
-
-import org.dasgupta.sample.springboot.mongodb.domain.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
