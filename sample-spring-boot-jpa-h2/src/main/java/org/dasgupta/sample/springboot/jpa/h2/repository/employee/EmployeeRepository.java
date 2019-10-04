1b1e428e8f74dd3400d0f439987fc465c69fea12
Debangshu Dasgupta <Debangshu.Dasgupta@gmail.com>
2019-10-03 23:00:46 -0400

    cleaned

--- sample-spring-boot-jpa-h2/src/main/java/org/dasgupta/sample/springboot/jpa/h2/repository/employee/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package org.dasgupta.sample.springboot.jpa.h2.repository.employee;
-
-import org.dasgupta.sample.springboot.jpa.h2.entity.employee.Employee;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends CrudRepository<Employee,Integer> {
-}
--- sample-spring-boot-jpa-h2/src/main/java/org/dasgupta/sample/springboot/jpa/h2/repository/product/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package org.dasgupta.sample.springboot.jpa.h2.repository.product;
-
-import org.dasgupta.sample.springboot.jpa.h2.entity.product.Product;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends CrudRepository<Product,Integer> {
-}
