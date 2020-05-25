578cf1f54e1586a032cc797a93c684023e719aa6
Debangshu Dasgupta <Debangshu.Dasgupta@gmail.com>
2020-05-25 17:33:47 -0400

    Updated

--- oauth-service/src/main/java/org/groodle/boot/service/oauth/Application.java
+++ oauth-service/src/main/java/org/groodle/boot/service/oauth/Application.java
@@ -2,10 +2,12 @@ package org.groodle.boot.service.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @EnableAuthorizationServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
