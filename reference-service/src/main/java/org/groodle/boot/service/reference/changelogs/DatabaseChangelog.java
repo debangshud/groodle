20d79ae1e28c740742f226927eb5ef66a721e187
Debangshu Dasgupta <Debangshu.Dasgupta@gmail.com>
2020-12-04 01:00:25 -0500

    added csv reading

--- reference-service/src/main/java/org/groodle/boot/service/reference/changelogs/DatabaseChangelog.java
+++ reference-service/src/main/java/org/groodle/boot/service/reference/changelogs/DatabaseChangelog.java
@@ -3,17 +3,33 @@ package org.groodle.boot.service.reference.changelogs;
 import com.github.cloudyrock.mongock.ChangeLog;
 import com.github.cloudyrock.mongock.ChangeSet;
 import com.github.cloudyrock.mongock.driver.mongodb.springdata.v2.decorator.impl.MongockTemplate;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.HeaderColumnNameMappingStrategy;
+import com.opencsv.exceptions.CsvException;
 import org.groodle.boot.service.reference.model.Country;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 @ChangeLog
 public class DatabaseChangelog {
 
     @ChangeSet(order = "001", id = "001", author = "Debangshu Dasgupta")
-    public void insert(MongockTemplate mongockTemplate){
-        Country country = new Country();
-        country.setName("India");
-        country.setAlpha2("IN");
-        country.setAlpha3("IND");
-        mongockTemplate.save(country);
+    public void insert(MongockTemplate mongockTemplate) throws IOException, CsvException {
+
+        CSVReader reader = new CSVReader(new InputStreamReader(new ClassPathResource("iso-country-codes.csv").getInputStream()));
+
+        HeaderColumnNameMappingStrategy<Country> beanStrategy = new HeaderColumnNameMappingStrategy<Country>();
+        beanStrategy.setType(Country.class);
+
+        CsvToBean<Country> csvToBean = new CsvToBean<Country>();
+        csvToBean.setCsvReader(reader);
+        csvToBean.setMappingStrategy(beanStrategy);
+
+        List<Country> countries = csvToBean.parse();
+        countries.forEach(mongockTemplate::save);
     }
 }
--- reference-service/src/main/java/org/groodle/boot/service/reference/model/Country.java
+++ reference-service/src/main/java/org/groodle/boot/service/reference/model/Country.java
@@ -1,5 +1,6 @@
 package org.groodle.boot.service.reference.model;
 
+import com.opencsv.bean.CsvBindByName;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
@@ -12,8 +13,13 @@ public class Country {
     @MongoId
     private String id;
     @Field("alpha-2")
+    @CsvBindByName(column = "alpha-2")
     private String alpha2;
+
+    @CsvBindByName(column = "alpha-3")
     @Field("alpha-3")
     private String alpha3;
+
+    @CsvBindByName(column = "name")
     private String name;
 }
